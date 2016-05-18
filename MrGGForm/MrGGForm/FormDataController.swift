f1de131bdd5432dfc0ce05a40a5d9dbb01b39b16
張子晏 <zhangziyan@zhangziyande-MacBook-Pro.local>
2016-05-18 17:06:28 +0800

    Update podfile Stevia. https://github.com/s4cha/Stevia

---
+++ MrGGForm/MrGGForm/FormDataController.swift
@@ -0,0 +1,19 @@
+//
+//  FormDataController.swift
+//  
+//
+//  Created by 張子晏 on 2016/5/18.
+//
+//
+
+import Foundation
+import SwiftyJSON
+
+func getJsonFormByFile() -> JSON {
+    if let path = NSBundle.mainBundle().pathForResource("FormStruct", ofType: "json") {
+        if let jsonData = NSData(contentsOfFile: path) {
+            return JSON(data: jsonData)
+        }
+    }
+    return nil
+}
--- MrGGForm/Pods/Target Support Files/Pods-MrGGForm/Pods-MrGGForm-frameworks.sh
+++ MrGGForm/Pods/Target Support Files/Pods-MrGGForm/Pods-MrGGForm-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/SteviaLayout/SteviaLayout.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/SteviaLayout/SteviaLayout.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
